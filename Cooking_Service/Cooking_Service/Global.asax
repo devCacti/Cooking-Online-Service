e0376e570a6b475d90a622c08ea32537874bb378
Tiago Laim <tiago.laim.rodrigues@gmail.com>
2024-01-01 02:58:05 +0000

    Versão: 0.1
    Creação do Serviço, ainda não funciona nada mas tem sistema de creação de contas, o que é bom.

---
+++ Cooking_Service/Cooking_Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cooking_Service.MvcApplication" Language="C#" %>
---
+++ Cooking_Service/Cooking_Service/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cooking_Service.Startup))]
+namespace Cooking_Service
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
