7fb9d0ed2626d57533d9ee6f289a553ee9824a20
Tiago Laim <tiago.laim.rodrigues@gmail.com>
2024-05-18 22:19:37 +0100

    0.1.2024.5.18.1
    Algumas alterações no HTML
    
    Mudança na identificação de versões:
    
    Concluido.Parte.Ano.Mes.Dia.IteraçãoDia

--- Cooking_Service/Cooking_Service/Controllers/HomeController.cs
+++ Cooking_Service/Cooking_Service/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace Cooking_Service.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
